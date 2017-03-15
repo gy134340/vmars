0e1e4f062384f6d22538ba655e5bf0fb87e89d28
gy134340 <guoyang134340@163.com>
2017-03-16 00:28:40 +0800

    Add date

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,25 +1,28 @@
 <template>
 	<div class="home" v-bind:style="bgall">
+		<h3 class="caption">每日天文图片</h3>
+		<!-- <p class="date">Select date...</p> -->
+		<input class="date" placeholder="Select date..."></input>
 		<div class="intro">
-			<h4 class="title">title: {{ apod.title}}</h4>
+			<h4 class="title">{{ apod.title}}</h4>
 			<p class="info">{{ apod.explanation }}</p>
 			<div class="more">
-				<p>author: {{ apod.copyright }}</p>
+				<p>author: {{ apod.copyright || "anonymous" }}</p>
 				<p>date: {{ apod.date }}</p>
 			</div>
-			
 		</div>
 	</div>
 </template>
 <script>
-
 import axios from 'axios';
+import Flatpickr from 'flatpickr';
 
 export default {
 	name: 'home',
 	data () {
 		return {
-			apod: ''
+			apod: '',
+			dateStr: '2016'
 		};
 	},
 	computed: {
@@ -32,14 +35,12 @@ export default {
 				'background-image': 'url(' + this.apod.url + ')',
 				'background-position': 'center center',
 				'background-repeat': 'no-repeat'
-			};
-			
+			};	
 		} 
 	},
 	components: {
-
+		
 	},
-
 	beforeMount () {
 		axios.get('https://api.nasa.gov/planetary/apod?api_key=NkLecWqIRgjAv1brKJLD1gvVekp48jnwv7H3NCeV')
 		.then(res => {
@@ -49,35 +50,71 @@ export default {
 			console.log(err);
 		});
 	},
+	mounted () {
+		let ele = document.querySelectorAll('.date')[0];
+		new Flatpickr(ele, {
+			enableTime: true,
+			maxDate: new Date(),
+			inline: false
+		});
+
+	},
 	methods: {
 
 	}
 };
 
 </script>
-<style >
+<style lang="scss" scoped>
+	.caption {
+		margin-top: 60px;
+		width: 100%;
+		color: #fff;
+		margin-bottom: 100px;
+		text-align: center;
+		position: absolute;
+
+	}
 	.home {
 		width: 100%;
 		height: 100%;
 		position: relative;
+		.intro {
+			display: block;
+			position: absolute;
+			width: 80%;
+			left: 50%;
+			top: 50%;
+			color: #fff;
+			-webkit-transform: translate(-50%, -50%);
+			transform: translate(-50%, -50%);
+		}
+		.date {
+			color: #ccc;
+			width: 130px;
+			height: 15px;
+			position: absolute;
+			margin: 30px 0px 0px 30px;
+			font-size: 14px;
+			display: inline-block;
+			
+			border-radius: 10px;
+			background-color: rgba(88,88,88,0.5);
+    		padding: 8px;
+		}
+		.flatpickr-calendar {
+			background-color: #484d5a;
+			box-shadow: none;
+		}
+		.title, .info, .more{
+			margin-bottom: 20px;
+		}
+		.more {
+			padding-right: 8px;
+			text-align: right;
+		}
 	}
-	.intro {
-		display: block;
-		position: absolute;
-		width: 80%;
-		left: 50%;
-		top: 50%;
-		color: #fff;
-		-webkit-transform: translate(-50%, -50%);
-		transform: translate(-50%, -50%);
-	}
-	.title, .info, .more{
-		margin-bottom: 20px;
-	}
-	.more {
-		padding-right: 8px;
-		text-align: right;
-	}
+	
 </style>
 
 
